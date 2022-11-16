8444b088db2562697620700b6fbf3b3e3262d4ef
Bruna Moraes <brunacm@id.uff.br>
2022-11-16 10:54:49 -0300

    feat(b-library): add and fixing routes

--- b-library/express-library/routes/index.js
+++ b-library/express-library/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page old. 
+*
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
+
+// GET home page.
+router.get('/', function (req, res) {
+  //res.send('funfo');
+  res.redirect('/catalog');
+});
 
 module.exports = router;
+
