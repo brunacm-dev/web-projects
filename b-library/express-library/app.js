0c76095775f888f17133a59cf5d6a0c519e610e3
Bruna Moraes <brunacm@id.uff.br>
2022-11-16 02:07:34 -0300

    feat(b-library): connection to mongodb with mongoose

--- b-library/express-library/app.js
+++ b-library/express-library/app.js
@@ -13,6 +13,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB =
+    "mongodb+srv://admin:admin@cluster0.kiaksl1.mongodb.net/local_library?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup, no caso o pug
 app.set('views', path.join(__dirname, 'views'));
