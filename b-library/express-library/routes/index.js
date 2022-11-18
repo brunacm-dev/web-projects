3d3a0ba7af226afa23a27e46ed725ad584d249c1
Bruna Moraes <brunacm@id.uff.br>
2022-11-18 14:33:34 -0300

    feat(b-library): fixing routes bug

--- b-library/express-library/routes/index.js
+++ b-library/express-library/routes/index.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page old. 
-*
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-*/
+// GET home page old. 
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-// GET home page.
+
+
+//GET home page.
 router.get('/', function (req, res) {
-  //res.send('funfo');
   res.redirect('/catalog');
 });
 
